855ab9b0e4decd42fb78f717835443b9f1936ed8
ImYuushi <kenleemori@yahoo.de>
2026-05-07 23:40:13 +0200

    Basic skeleton for harness instrumented

---
+++ fuzzer-instrumented/src/harness.c
@@ -0,0 +1,18 @@
+#include <png.h>
+
+
+
+int main(int argc, char *argv[]) {
+    // Get the path into a variable
+
+    // Read file into png_file (or at least get a pointer to it?)
+
+    // initialize png_struct or pointer to it?
+    
+    // Initialize png_info or pointer to it?
+
+    // handle setjmp with invalid inputs
+
+    // clean up because theres no trashman in C
+
+}
