cdfabb154d1e815c3681cef973602c059ef8c760
ImYuushi <kenleemori@yahoo.de>
2026-05-11 23:20:04 +0200

    fixed run script and added plotting functionality

--- fuzzer-instrumented/src/run_fuzzer_withFlags.sh
+++ fuzzer-instrumented/src/run_fuzzer_withFlags.sh
@@ -10,14 +10,18 @@ DURATION="${2:-100}"
 SEEDS_DIR="/fuzzing/seeds"
 OUT_DIR="/fuzzing/instrumented/findings/$METHOD"
 BINARY="/fuzzing/instrumented/src/fuzz_bins/fuzz_fuzz_$METHOD"
+DICTS="/fuzzing/dict/png.dict"
+PLOT_DIR="/fuzzing/instrumented/plot-output/$METHOD"
 mkdir -p "$OUT_DIR"
-afl-fuzz -V "$DURATION" -i "$SEEDS_DIR" -o "$OUT_DIR" -- "$BINARY" @@ 
-
+mkdir -p PLOT_DIR
+afl-fuzz -V "$DURATION" -i "$SEEDS_DIR" -o "$OUT_DIR" -x "$DICTS" -- "$BINARY" @@ 
+afl-plot  "$OUT_DIR" "$PLOT_DIR"
 set -e
 
 cleanup() {
   echo "[+] fixing ownership..."
   chown -R ${HOST_UID:-1000}:${HOST_GID:-1000} /fuzzing/instrumented/findings || true
+  chown -R ${HOST_UID:-1000}:${HOST_GID:-1000} /fuzzing/instrumented/plot_output || true
 }
 
 trap cleanup EXIT
